972f1b11c204717578c8af71a44353316a866e63
Colin Jaffe <balloonasaurus@gmail.com>
2021-12-07 15:16:18 -0500

    More testing.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hiiiiii</p>
+        <p>Well hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
